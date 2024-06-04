b03eaa453848f81b076bf3b804f4a18d8bdcfb8d
ShibabOfficial <aleksanderszp2016@gmail.com>
2024-06-04 23:47:37 +0200

    big optimalizations

--- main.cpp
+++ main.cpp
@@ -8,54 +8,6 @@
 
 using namespace std;
 
-unsigned int microsecond = 1000000;
-
-/*
-\033[38;2;<r>;<g>;<b>m   Select RGB foreground color
-\033[48;2;<r>;<g>;<b>m   Select RGB background color
-*/
-
-class trail {
-    public:
-        int x;
-        int y;
-        vector<char> chl;
-        
-        trail(int x, int y) {
-            this->x = x;
-            this->y = y;
-        }
-};
-
-char inpchar() {
-    char ch;
-    struct termios oldt, newt;
-    int oldf;
-
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
-
-    ch = getchar();
-
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    fcntl(STDIN_FILENO, F_SETFL, oldf);
-
-    return ch;
-}
-
-void color(bool fob, int r, int g, int b) {
-    if (fob) {
-        printf("\033[38;2;%d;%d;%dm", r, g, b);
-        return;
-    }
-
-    printf("\033[48;2;%d;%d;%dm", r, g, b);
-}
-
 void clear(winsize& w) {
     system("clear");
     for (int i = 0; i < w.ws_row - 1; i++) {
@@ -64,85 +16,114 @@ void clear(winsize& w) {
 }
 
 int main(int argc, char* argv[]) {
+    // setting up input
     system("stty raw");
+    struct termios oldt, newt;
+    int oldf;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+    
     cout << "\x1b[?25l" << flush; // hide cursor
-    color(true, 255, 255, 255);
 
+    // terminal sizing
     struct winsize w;
     struct winsize wl;
+    
+    ioctl(0, TIOCGWINSZ, &w);
+
     clear(w);
-    int frame = 0;
 
+    // speed
     int speed = 30;
-
     if (argc >= 2) {
         speed = stoi(argv[1]);
     }
 
+    // trails stuff
     int length = 0;
-    vector<trail*> trails;
+    vector<pair<int, int> > trails;
+    vector<char> lastchars;
 
+    // rng
     random_device dev;
     mt19937 rng(dev());
+
+    printf("\033[38;2;%d;%d;%dm", 255, 255, 255); // color to white
     
     while (true) {
         ioctl(0, TIOCGWINSZ, &w);
 
+        // size changed
         if (w.ws_col != wl.ws_col || w.ws_row != wl.ws_row) {
             clear(w);
+
             trails.clear();
+            lastchars.clear();
+
             wl = w;
+            ioctl(0, TIOCGWINSZ, &w);
+
+            length = w.ws_row / 2;
             continue;
         }
 
-        if (inpchar() == 'q') break;
+        if (getchar() == 'q') break;
 
-        frame++;
-        length = w.ws_row / 2;
-
+        // gen new numbers
         uniform_int_distribution<mt19937::result_type> xrandom(0, w.ws_col);
         uniform_int_distribution<mt19937::result_type> yrandom(-length + 2, 0);
 
-        trails.push_back(new trail(xrandom(rng), yrandom(rng)));
-        trails.push_back(new trail(xrandom(rng), yrandom(rng)));
+        // push first random trail
+        trails.push_back(make_pair(xrandom(rng), yrandom(rng)));
+        lastchars.push_back(' ');
+        //push second random trail
+        trails.push_back(make_pair(xrandom(rng), yrandom(rng)));
+        lastchars.push_back(' ');
 
         for (int i = 0; i < trails.size(); i++) {
-            char current = (char)rand() % 26 + 97;
-
-            if (trails[i]->chl.size() > length)
-                trails[i]->chl.erase(trails[i]->chl.begin() + length, trails[i]->chl.end());
-
-            if (trails[i]->y - length > w.ws_row) {
+            // if the trail is out of bounds, delete it
+            if (trails[i].second - length > w.ws_row) {
                 trails.erase(trails.begin() + i);
                 continue;
             }
 
-            if (trails[i]->y - length - 1 > 0)
-                printf("\033[%d;%dH%c\n", trails[i]->y - length - 1, trails[i]->x, ' ');
+            // clear last char if it was inbounds
+            if (trails[i].second - length - 1 > 0)
+                printf("\033[%d;%dH%c\n", trails[i].second - length - 1, trails[i].first, ' ');
 
-            color(true, 30, 180, 60);
-            for (int j = 0; j < length; j++) {
-                if (trails[i]->y - j - 1 < 0) break;
-                if (trails[i]->y - j - 1 >= w.ws_row) continue;
-
-                printf("\033[%d;%dH%c\n", trails[i]->y - j - 1, trails[i]->x, trails[i]->chl.at(j));
-            }
-            
-            if (trails[i]->y < w.ws_row && trails[i]->y > 0) {
-                color(true, 255, 255, 255);
-                printf("\033[%d;%dH%c\n", trails[i]->y, trails[i]->x, current);
+            // change the color of the last char to green if its inbounds
+            if (trails[i].second - 1 > 0 && trails[i].second - 1 < w.ws_row) {
+                printf("\033[38;2;%d;%d;%dm", 30, 180, 60); // color to green
+                printf("\033[%d;%dH%c\n", trails[i].second - 1, trails[i].first, lastchars[i]); // we need to print it again
             }
 
-            trails[i]->y++;
-            trails[i]->chl.insert(trails[i]->chl.begin(), current);
+            // print new char if its inbounds
+            if (trails[i].second < w.ws_row && trails[i].second > 0) {
+                char current = (char)rand() % 26 + 97; // gen random char
+                printf("\033[38;2;%d;%d;%dm", 255, 255, 255); // color to white
+                printf("\033[%d;%dH%c\n", trails[i].second, trails[i].first, current);
+                lastchars[i] = current;
+            }
+
+            trails[i].second++;
         }
 
         wl = w;
-        usleep(100000 - speed * 1000);
+
+        usleep(100000 - speed * 1000); // frame timing
     }
 
-    system("clear");
+    // restoring input
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
     system("stty cooked");
+
+    system("clear");
+
     cout << "\033[0m" << flush; // reset colors
     cout << "\x1b[?25h" << flush; // show cursor
     return 0;
